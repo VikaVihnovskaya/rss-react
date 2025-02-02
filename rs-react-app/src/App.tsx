678d222dd9f33b4f4957638ec3628a1e21901327
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2025-02-03 00:23:31 +0300

    feat: implement search page and searching functionality

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,35 +1,159 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_BASE_URL = 'https://swapi.dev/api/films/';
+const API_SEARCH_URL = 'https://swapi.dev/api/films/?search';
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Item {
+  title: string;
+  director: string;
+  episode_id: number;
 }
 
-export default App
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps> {
+  state = { hasError: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: unknown, errorInfo: unknown) {
+    console.error('Caught error:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="error-container">
+          <p>Something went wrong.</p>
+          <button onClick={() => window.location.reload()}>Reload</button>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+class App extends React.Component<
+  unknown,
+  { searchTerm: string; items: Item[]; loading: boolean; error: string | null }
+> {
+  constructor(props: unknown) {
+    super(props);
+    const savedSearch = localStorage.getItem('searchTerm') || '';
+    this.state = {
+      searchTerm: savedSearch,
+      items: [],
+      loading: false,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData(this.state.searchTerm);
+  }
+
+  fetchData = async (query: string) => {
+    this.setState({ loading: true, error: null });
+    try {
+      const searchQuery = query.trim();
+      let response;
+      if (searchQuery == '') {
+        response = await fetch(`${API_BASE_URL}`);
+      } else {
+        response = await fetch(`${API_SEARCH_URL}=${searchQuery}`);
+      }
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      } else {
+        const data = await response.json();
+        const items: Item[] = data.results as Item[];
+        this.setState({
+          searchTerm: query.trim(),
+          items: items,
+          error: null,
+          loading: false,
+        });
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        this.setState({ error: err.message });
+      } else {
+        this.setState({ error: 'An unknown error occurred' });
+      }
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  handleSearch = () => {
+    const trimmedSearch = this.state.searchTerm.trim();
+    localStorage.setItem('searchTerm', trimmedSearch);
+    this.fetchData(trimmedSearch);
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <ErrorBoundary>
+        <div className="container">
+          <div className="search-bar">
+            <input
+              value={this.state.searchTerm}
+              onChange={this.handleChange}
+              placeholder="Search..."
+            />
+            <button onClick={this.handleSearch}>Search</button>
+          </div>
+          <div className="results-container">
+            {this.state.loading ? (
+              <div className="loading">Loading...</div>
+            ) : this.state.error ? (
+              <div className="error">{this.state.error}</div>
+            ) : (
+              <div>
+                <div className="result-header">
+                  <span className="header-title">Movie Title</span>
+                  <span className="header-director">Director</span>
+                </div>
+                {this.state.items.length > 0 ? (
+                  this.state.items.map((item) => (
+                    <div key={item.episode_id} className="result-item">
+                      <span className="movie-title">{item.title}</span>
+                      <span className="movie-director">({item.director})</span>
+                    </div>
+                  ))
+                ) : (
+                  <p>No results found.</p>
+                )}
+              </div>
+            )}
+          </div>
+          <button
+            className="error-button"
+            onClick={() => {
+              try {
+                throw new Error('Test Error is thrown');
+              } catch (error) {
+                console.error('Caught error:', error);
+                this.setState({ error: 'An test error is thrown.' });
+              }
+            }}
+          >
+            Throw Error
+          </button>
+        </div>
+      </ErrorBoundary>
+    );
+  }
+}
+
+export default App;
