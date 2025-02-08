744ea54040a6ac2826981bed344792bce6de0d4a
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2025-02-08 20:06:29 +0300

    feat: add routing

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 import Searcher from './components/Searcher/Searcher.tsx';
 
 const App: React.FC = () => {
   return (
-    <ErrorBoundary>
-      <Searcher />
-    </ErrorBoundary>
+    <Router>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<Searcher />} />
+        </Routes>
+      </ErrorBoundary>
+    </Router>
   );
 };
 
