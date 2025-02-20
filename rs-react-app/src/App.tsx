9adf865c1f5fe2925df21866c347eb668c3a5056
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2025-02-20 23:06:54 +0300

    feat: add togle theme

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { ThemeProvider } from './context/ThemeContext.tsx';
+import ThemeSelector from './components/ThemeSelector/ThemeSelector.tsx';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 import Searcher from './components/Searcher/Searcher.tsx';
 import NotFoundPage from './components/NotFoundPage/NotFoundPage.tsx';
@@ -9,10 +11,13 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <ErrorBoundary>
-        <Routes>
-          <Route path="/*" element={<Searcher />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <ThemeProvider>
+          <ThemeSelector />
+          <Routes>
+            <Route path="/*" element={<Searcher />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </ThemeProvider>
       </ErrorBoundary>
     </BrowserRouter>
   );
