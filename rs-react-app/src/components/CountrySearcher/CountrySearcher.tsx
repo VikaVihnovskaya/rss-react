57846d938b19e2c60e2b3ca50419154558e1f300
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2025-03-23 12:42:52 +0300

    feat: add component CountrySearcher

--- rs-react-app/src/components/CountrySearcher/CountrySearcher.tsx
+++ rs-react-app/src/components/CountrySearcher/CountrySearcher.tsx
@@ -1,44 +1,6 @@
 import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import SearchInput from '../SearchInput/SearchInput';
-
-interface Country {
-  name: {
-    common: string;
-  };
-  population: number;
-  region: string;
-  flags: {
-    svg: string;
-  };
-  cca3: string;
-}
-
-interface CountryCardProps {
-  country: Country;
-  isVisited: boolean;
-  onClick: (countryName: string) => void;
-}
-
-const CountryCard: React.FC<CountryCardProps> = React.memo(
-  ({ country, isVisited, onClick }) => {
-    return (
-      <div
-        onClick={() => onClick(country.name.common)}
-        data-country-name={country.name.common}
-        className={isVisited ? 'country-card visited' : 'country-card'}
-      >
-        <h3>{country.name.common}</h3>
-        <img
-          src={country.flags.svg}
-          alt={`${country.name.common} flag`}
-          width={100}
-        />
-        <p>Population: {country.population}</p>
-        <p>Region: {country.region}</p>
-      </div>
-    );
-  }
-);
+import CountryCard, { Country} from '../CountryCard/CountryCard';
 
 const App: React.FC = () => {
   const [countries, setCountries] = useState<Country[]>([]);
@@ -117,7 +79,6 @@ const App: React.FC = () => {
       <h1>Country Explorer</h1>
 
       <div>
-
         <SearchInput
           searchTerm={searchTerm}
           onSearchTermChange={setSearchTerm}
